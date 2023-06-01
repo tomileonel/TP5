f1f9426770f776cdf861576a76f96810a30ddcbc
TOMÁS LEONEL DEGESE <47349708@ort>
2023-06-01 15:28:27 -0300

    fin clase 1

--- TP5/Models/Escape.cs
+++ TP5/Models/Escape.cs
@@ -1,5 +1,5 @@
 public static class Escape{
-public static string[] incognitasSalas{get;private set;} = new string[4] {"a","b","c","d"} ;
+public static string[] incognitasSalas{get;private set;} = new string[4] {"10","ANUBIS","HARPO EGIPCIO","WWWDSSDWWDSSDWAWASSAWWWDSDSS"} ;
 static int estadoJuego{get;set;} = 1;
 
 public static int GetEstadoJuego(){
